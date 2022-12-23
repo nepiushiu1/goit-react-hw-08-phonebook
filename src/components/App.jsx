f6d402d6f3829782108366c624a84274bdc1c0cd
Сергей Власенко <98455988+nepiushiu1@users.noreply.github.com>
2022-12-23 18:57:23 +0200

    не отправляется запрос

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-// import { Contacts } from 'pages/contacts';
-import { Home } from 'pages/Home';
-import { Login } from 'pages/Login';
-import { Register } from 'pages/Register';
+import { Contacts } from 'pages/Contacts/Contacts';
+import { Home } from 'pages/Home/Home';
+import { Login } from 'pages/Login/Login';
+import { Register } from 'pages/Register/Register';
 import { Route, Routes } from 'react-router-dom';
 import { AppBar } from './AppBar/AppBar';
-import ContactForm from './ContactForm/ContactForm';
+// import ContactForm from './ContactForm/ContactForm';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<AppBar />}>
         <Route index element={<Home />} />
-        <Route path="contacts" element={<ContactForm />} />
+        <Route path="contacts" element={<Contacts />} />
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
       </Route>
---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { useDispatch } from 'react-redux';
+// import { selectIsLoading, selectError } from 'redux/contacts/selector';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contact/operations';
+
+import Filter from 'components/Filter/Filter';
+import ContactForm from 'components/ContactForm/ContactForm';
+// import ContactList from 'components/ContactList/ContactList';
+
+export const Contacts = () => {
+  // const isLoading = useSelector(selectIsLoading);
+  // const error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        {/* {isLoading && !error && <Loader />} */}
+        {/* {!isLoading && !error && <ContactList />} */}
+      </div>
+    </>
+  );
+};
